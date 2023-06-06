3b79a05fb346a5171989a09487b5e5a1c0befc33
Nonhle <nonhlemsomi9@gmail.com>
2023-06-06 12:09:01 -0700

    task for error fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - releases a linked list
- * @head: listint_t list to be released
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
